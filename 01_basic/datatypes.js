71f847e1bd812a961bf2084a1968082a00febcde
ayushmaurya857 <ayushmaurya857@gmail.com>
2025-09-22 21:45:06 +0000

    devcontainer and some basic files

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") //we are using nodejs, not brower
+
+let name = "Ayush"
+let age = 20
+let isloggedIn = false
+
+// number => 2 to power 53 range 
+// bigint 
+// string =>""
+// boolean => true/false
+// null => stand alone value
+// undefined => value is not asign
+// symbol => unique
+
+// object
+
+console.log(typeof null) //output is object
+console.log(typeof undefined) //output is undefined
+
+
+
---
+++ 01_basic/variables.js
@@ -0,0 +1,6 @@
+const accountId = 12342
+let accountEmail = "ayush@gmail.com"
+var accountPass = "21323"
+console.log(accountId); 
+console.table([accountEmail, accountId, accountPass])
+//prefer not to use var because of issue in block scope and functional scope
